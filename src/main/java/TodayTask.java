c3a3b22f179063103dc01c4b6936638560e6bc80
Vishalraj2000 <knvishalraj05@gmail.com>
2022-04-20 23:13:42 +0530

    new

--- src/main/java/TodayTask.java
+++ src/main/java/TodayTask.java
@@ -3,8 +3,7 @@ public class TodayTask {
 
 	public static void main(String[] args) {
 		System.out.println("Test 1");
-		System.out.println("Test 2");
-		System.out.println("Ajith branch");
+		System.out.println("Test 9");
 		System.out.println("Test 7");
 	}
 		
